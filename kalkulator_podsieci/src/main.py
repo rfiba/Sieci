edce5024abd5581546eb262bd9f04bab04c9f650
rfiba <rafal.fiba@hotmail.com>
2018-03-21 18:23:17 +0100

    Pobieranie z wiersza lub ip komputera. Formatowanie pobranego ip komputera

--- kalkulator_podsieci/src/main.py
+++ kalkulator_podsieci/src/main.py
@@ -5,6 +5,17 @@ import sys
 import json
 import socket
 
+def checkAddress(address):
+    if(address[len(address)-3] =='\\'):
+        
+    return
+
 if(len(sys.argv) == 1):
     ip = socket.gethostbyaddr(socket.gethostname())
-print (ip[2])
+    ip = str(ip[2])
+    ip = ip.strip('[]\'')
+    print("Your ip adress:", ip)
+    a = ip.split('.')
+    print (a)
+else:
+    print( sys.argv[1])
