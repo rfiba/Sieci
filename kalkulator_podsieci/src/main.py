999a91a8e117c23c0b8618acfe5a87cbfabd9a35
rfiba <rafal.fiba@hotmail.com>
2018-03-22 16:20:41 +0100

    split działa i checkowanie czy numer

--- kalkulator_podsieci/src/main.py
+++ kalkulator_podsieci/src/main.py
@@ -4,18 +4,33 @@
 import sys
 import json
 import socket
+import re
 
 def checkAddress(address):
-    if(address[len(address)-3] =='\\'):
-        
-    return
+    length = len(address)
+    if(length != 4 | length != 5):
+        return False;
+
+    if(length == 5):
+        for i in range(length):
+            if (str(address[i]).isdigit() == False):
+                return False;
+
+    return True;
 
 if(len(sys.argv) == 1):
     ip = socket.gethostbyaddr(socket.gethostname())
     ip = str(ip[2])
     ip = ip.strip('[]\'')
+    ip += '\\32'
     print("Your ip adress:", ip)
-    a = ip.split('.')
+    a = ip.split(r'.')
     print (a)
+    #for i in range(a):
+    b = str(a[3]).split('\\')
+
+    del a[-1]
+    print (a+b)
+    print(checkAddress(a+b))
 else:
     print( sys.argv[1])
